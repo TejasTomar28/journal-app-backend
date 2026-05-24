51f97ef60da79ed5086b042f9c13efdd9e6fb818
TejasTomar28 <tomar.tejas28@gmail.com>
2026-05-24 13:03:59 +0530

    added e-mailing feature

---
+++ src/main/java/com/tejastomar/journalapp/repository/UserRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.tejastomar.journalapp.repository;
+
+import com.tejastomar.journalapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUserForSA() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        query.addCriteria(Criteria.where("roles").in("USER","ADMIN"));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
+    }
+}
---
+++ src/test/java/com/tejastomar/journalapp/service/EmailServiceTests.java
@@ -0,0 +1,18 @@
+package com.tejastomar.journalapp.service;
+
+import com.tejastomar.journalapp.services.EmailService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail(){
+        emailService.sendEmail("tomar.tejas28@gmail.com","Testing java mail sender", "aap kaise hain?");
+    }
+}
